bc00466d64c54cb30ceda5083f674e5617bf8437
railken <railken@protonmail.ch>
2019-01-21 16:19:32 +0100

    cleanup

--- config/amethyst.alias.data.alias.php
+++ config/amethyst.alias.data.alias.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    'table'       => 'amethyst_aliases',
-    'comment'     => 'Alias',
-    'model'       => Railken\Amethyst\Models\Alias::class,
-    'schema'      => Railken\Amethyst\Schemas\AliasSchema::class,
-    'repository'  => Railken\Amethyst\Repositories\AliasRepository::class,
-    'serializer'  => Railken\Amethyst\Serializers\AliasSerializer::class,
-    'validator'   => Railken\Amethyst\Validators\AliasValidator::class,
-    'authorizer'  => Railken\Amethyst\Authorizers\AliasAuthorizer::class,
-    'faker'       => Railken\Amethyst\Fakers\AliasFaker::class,
-    'manager'     => Railken\Amethyst\Managers\AliasManager::class,
-    'aliasables'  => [
+    'table'      => 'amethyst_aliases',
+    'comment'    => 'Alias',
+    'model'      => Railken\Amethyst\Models\Alias::class,
+    'schema'     => Railken\Amethyst\Schemas\AliasSchema::class,
+    'repository' => Railken\Amethyst\Repositories\AliasRepository::class,
+    'serializer' => Railken\Amethyst\Serializers\AliasSerializer::class,
+    'validator'  => Railken\Amethyst\Validators\AliasValidator::class,
+    'authorizer' => Railken\Amethyst\Authorizers\AliasAuthorizer::class,
+    'faker'      => Railken\Amethyst\Fakers\AliasFaker::class,
+    'manager'    => Railken\Amethyst\Managers\AliasManager::class,
+    'aliasables' => [
         Railken\Amethyst\Models\Foo::class => Railken\Amethyst\Managers\FooManager::class,
     ],
 ];
--- config/amethyst.alias.http.admin.alias.php
+++ config/amethyst.alias.http.admin.alias.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'enabled'     => true,
-    'controller'  => Railken\Amethyst\Http\Controllers\Admin\AliasesController::class,
-    'router'      => [
-        'as'        => 'alias.',
-        'prefix'    => '/aliases',
+    'enabled'    => true,
+    'controller' => Railken\Amethyst\Http\Controllers\Admin\AliasesController::class,
+    'router'     => [
+        'as'     => 'alias.',
+        'prefix' => '/aliases',
     ],
 ];
