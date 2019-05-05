092ae33d0a6259d82de0ad1b409c608f2973b87a
railken <railken@protonmail.ch>
2019-05-06 00:27:02 +0200

    fix style

--- src/Providers/AliasServiceProvider.php
+++ src/Providers/AliasServiceProvider.php
@@ -13,16 +13,15 @@ class AliasServiceProvider extends CommonServiceProvider
     public function register()
     {
         parent::register();
-
     }
-    
+
     /**
      * @inherit
      */
     public function boot()
     {
         parent::boot();
-        
+
         \Illuminate\Database\Eloquent\Builder::macro('aliases', function (): MorphMany {
             return app('amethyst')->createMacroMorphRelation($this, \Railken\Amethyst\Models\Alias::class, 'aliases', 'aliasable');
         });
