c28afd331cc4084ba7df5b595bfd71cc61f65e0f
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/AliasManager.php
+++ src/Managers/AliasManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Alias newEntity()
+ * @method \Amethyst\Schemas\AliasSchema getSchema()
+ * @method \Amethyst\Repositories\AliasRepository getRepository()
+ * @method \Amethyst\Serializers\AliasSerializer getSerializer()
+ * @method \Amethyst\Validators\AliasValidator getValidator()
+ * @method \Amethyst\Authorizers\AliasAuthorizer getAuthorizer()
+ */
 class AliasManager extends Manager
 {
     use ConfigurableManager;
